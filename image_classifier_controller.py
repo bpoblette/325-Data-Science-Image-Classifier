ede5757452e4fc00bb9998c7a1a98634fdb7d9ad
bpoblette <bpoblette@zagmail.gonzaga.edu>
2025-03-03 14:48:20 -0800

    trying deployment again

--- image_classifier_controller.py
+++ image_classifier_controller.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile, File
 from image_classifier import ImageClassifier
 from io import BytesIO
 from PIL import Image
@@ -7,16 +7,16 @@ import cv2
 
 app = FastAPI()
 
-# to do: Need to take image
-@app.get("/predict/")
-async def predict(file = UploadFile()):
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):  
     image_classifier = ImageClassifier()
 
+    # Read the image file
     image_bytes = await file.read()
-    image = Image.open(BytesIO(image_bytes).convert("RGB"))
+    image = Image.open(BytesIO(image_bytes)).convert("RGB")
     image = np.array(image)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
+    # Get prediction
     result = image_classifier.predict(image=image)
     return {"filename": file.filename, "predictions": result}
-    
