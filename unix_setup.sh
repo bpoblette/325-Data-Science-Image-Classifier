ee9f6b0387cb6de6b29caed52cc689db7f218d34
bpoblette <bpoblette@zagmail.gonzaga.edu>
2025-03-03 16:00:48 -0800

    got rid of cv2 for production

--- unix_setup.sh
+++ unix_setup.sh
@@ -6,7 +6,4 @@ git pull
 echo "Installing dependencies from requirements.txt..."
 pip install -r requirements.txt
 
-echo "Updating requirements.txt with currently installed packages..."
-pip freeze > requirements.txt
-
 echo "Setup complete!"
